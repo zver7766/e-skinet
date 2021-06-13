118504b902d203e759b76b81e088e72d48a7e092
Max Prokhorchuk <star32975@gmail.com>
2021-06-13 19:35:56 +0300

    Added eager loading for products

--- Core/Intefraces/IProductRepository.cs
+++ Core/Intefraces/IProductRepository.cs
@@ -8,5 +8,9 @@ namespace Core.Intefraces
     {
         Task<Product> GetProductByIdAsync(int id);
         Task<IReadOnlyList<Product>> GetProductsAsync();
+        
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+        
+        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
     }
 }
