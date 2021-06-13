35965bb9cdf715e8d63403f330fba9e68ead8297
Max Prokhorchuk <star32975@gmail.com>
2021-06-13 19:17:01 +0300

    Added jsons for seeding data into SQLite and implement its seeding (Program.cs)

--- API/Program.cs
+++ API/Program.cs
@@ -14,7 +14,7 @@ namespace API
         public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
-            using(var scope = host.Services.CreateScope())
+            using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
                 var loggerFactory = services.GetRequiredService<ILoggerFactory>();
@@ -22,8 +22,9 @@ namespace API
                 {
                     var context = services.GetRequiredService<StoreContext>();
                     await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context, loggerFactory);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     var logger = loggerFactory.CreateLogger<Program>();
                     logger.LogError(ex, "An error occured during migration");
