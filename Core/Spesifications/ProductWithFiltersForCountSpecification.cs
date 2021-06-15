be73050143f4a9b598a8ab7500e51a8260ac2fc4
Max Prokhorchuk <star32975@gmail.com>
2021-06-15 22:29:33 +0300

    Added search product by name using query string

--- Core/Spesifications/ProductWithFiltersForCountSpecification.cs
+++ Core/Spesifications/ProductWithFiltersForCountSpecification.cs
@@ -6,11 +6,12 @@ namespace Core.Spesifications
     {
         public ProductWithFiltersForCountSpecification(ProductSpecParams productParams)
              : base(x =>
-            (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
-            (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
+                 (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains
+                 (productParams.Search)) &&
+                 (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+                 (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
         )
         {
-            
         }
 
     }
--- Core/Spesifications/ProductsWIthTypesAndBrandsSpecification.cs
+++ Core/Spesifications/ProductsWIthTypesAndBrandsSpecification.cs
@@ -7,6 +7,8 @@ namespace Core.Spesifications
 
         public ProductsWIthTypesAndBrandsSpecification(ProductSpecParams productParams)
             : base(x => 
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains
+                (productParams.Search)) &&
                 (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && 
                 (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
             )
