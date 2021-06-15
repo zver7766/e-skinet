c3e4c7e85c0e6230d06e24772e33c379eaac3a1c
Max Prokhorchuk <star32975@gmail.com>
2021-06-15 22:34:17 +0300

    Registred  CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,14 @@ namespace API
 
             services.AddSwaggerDocumentation();
 
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +57,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
