dd6eec799b9614d81d9302a8af6fffa03781149c
Max Prokhorchuk <star32975@gmail.com>
2021-06-19 13:15:49 +0300

    Styled product-detail (simple for now)

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
+  product: IProduct;
 
-  constructor() { }
+  constructor(private shopService: ShopService, private activateRoute : ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(product => {
+      this.product = product;
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
