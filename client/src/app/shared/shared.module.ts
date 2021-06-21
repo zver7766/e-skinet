3d6655ff780ae960ef89dd9190a02f581d9877d5
Max Prokhorchuk <star32975@gmail.com>
2021-06-21 15:31:52 +0300

    Added the home page content + some styles fixes

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {PaginationModule} from 'ngx-bootstrap/pagination';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel'
 
 @NgModule({
   declarations: [PagingHeaderComponent, PagerComponent],
   imports: [
     CommonModule,
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   exports: [
     PaginationModule,
     PagingHeaderComponent,
-    PagerComponent
+    PagerComponent,
+    CarouselModule
   ]
 })
 export class SharedModule { }
