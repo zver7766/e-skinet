64bc3b07487c9b09b0df4cb123105114560c5bd1
Max Prokhorchuk <star32975@gmail.com>
2021-06-28 15:58:17 +0300

    Added checkout module & routing to this

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
     path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule)
     , data: { breadcrumb: 'Basket' }
   },
+  {
+    path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule)
+    , data: { breadcrumb: 'Checkout' }
+  },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ];
 
