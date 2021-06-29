1c6db2c066abcdcd978ac47e27dc0cef372fa8d3
Max Prokhorchuk <star32975@gmail.com>
2021-06-29 10:20:48 +0300

    Addded identity db class & registred it into startup

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,7 @@ using API.Helpers;
 using API.Middleware;
 using API.Extensions;
 using StackExchange.Redis;
+using Infrastructure.Identity;
 
 namespace API
 {
@@ -29,6 +30,11 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
                     x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<AppIdentityDbContext>(x =>
+            {
+                x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
+            });
 
             services.AddSingleton<IConnectionMultiplexer>(c =>
             {
