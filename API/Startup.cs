57b6bf097fc4903bdc5d672bb765915c5573fbce
Max Prokhorchuk <star32975@gmail.com>
2021-06-29 12:12:17 +0300

    Registred the requiered minimum for working Identity

--- API/Startup.cs
+++ API/Startup.cs
@@ -45,6 +45,8 @@ namespace API
 
             services.AddApplicationServices();
 
+            services.AddIdentityServices();
+
             services.AddSwaggerDocumentation();
 
             services.AddCors(opt =>
