3163a930c8e4e052262f86bcb0a4db15fcf2c433
Max Prokhorchuk <star32975@gmail.com>
2021-07-03 18:29:29 +0300

    Added account module,routes,service and base methods to it, registred routes, created models

---
+++ client/src/app/account/account.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { IUser } from '../shared/models/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = environment.apiUrl;
+  private currentUserSource = new BehaviorSubject<IUser>(null);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private hhtp: HttpClient, private router: Router) { }
+
+  login(values: any) {
+    return this.hhtp.post(this.baseUrl + 'account/login', values).pipe(
+      map((user: IUser) => {
+        localStorage.setItem('token', user.token);
+        this.currentUserSource.next(user);
+      })
+    );
+  }
+
+  register(values: any) {
+    return this.hhtp.post(this.baseUrl + 'account/register', values).pipe(
+      map((user: IUser) => {
+        if (user) {
+          localStorage.setItem('token', user.token);
+        }
+      })
+    )
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.currentUserSource.next(null);
+    this.router.navigateByUrl('/');
+  }
+
+  checkEmailExists(email: string){
+    return this.hhtp.get(this.baseUrl + '/account/emailexists?email=' + email);
+  }
+
+}
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -22,6 +22,10 @@ const routes: Routes = [
     path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule)
     , data: { breadcrumb: 'Checkout' }
   },
+  {
+    path: 'account', loadChildren: () => import('./account/account.module').then(mod => mod.AccountModule)
+    , data: { breadcrumb: {skip: true} }
+  },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
 ];
 
