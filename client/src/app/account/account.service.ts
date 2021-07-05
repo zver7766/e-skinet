1cf697ce875255a65d5d5d26c2f3044fe0e4e960
Max Prokhorchuk <star32975@gmail.com>
2021-07-05 09:28:50 +0300

    Presisting the login

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
@@ -15,6 +15,24 @@ export class AccountService {
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private hhtp: HttpClient, private router: Router) { }
+
+  getCurrentUserValue() {
+    return this.currentUserSource.value;
+  }
+
+  loadCurrentUser(token: string){
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', `Bearer ${token}`);
+
+    return this.hhtp.get(this.baseUrl + 'account', {headers}).pipe(
+      map((user: IUser) =>{
+        if (user) {
+          localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
 
   login(values: any) {
     return this.hhtp.post(this.baseUrl + 'account/login', values).pipe(
@@ -41,7 +59,7 @@ export class AccountService {
     this.router.navigateByUrl('/');
   }
 
-  checkEmailExists(email: string){
+  checkEmailExists(email: string) {
     return this.hhtp.get(this.baseUrl + '/account/emailexists?email=' + email);
   }
 
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
@@ -9,16 +10,33 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'SkiNet';
 
-  constructor(private basketService: BasketService) { }
+  constructor(private basketService: BasketService, private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
       this.basketService.getBasket(basketId).subscribe(() => {
         console.log('initialized basket');
-      }, error =>{
+      }, error => {
         console.log(error);
       });
     }
   }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.accountService.loadCurrentUser(token).subscribe(() => {
+        console.log('loaded user');
+      }, error => {
+        console.log(error);
+      })
+    }
+  }
+
 }
