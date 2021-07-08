e9921293c475737a3da5aaaf41385b609877ff3f
Max Prokhorchuk <star32975@gmail.com>
2021-07-08 15:53:09 +0300

    Updated the generic repository

--- Core/Intefraces/IGenericRepository.cs
+++ Core/Intefraces/IGenericRepository.cs
@@ -12,5 +12,8 @@ namespace Core.Intefraces
         Task<T> GetEntityWithSpec(ISpecification<T> spec);
         Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
         Task<int> CountAsync(ISpecification<T> spec);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
