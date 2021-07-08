d6bb2e9a44f8910885ea8c8988c5cc4ba418d473
Max Prokhorchuk <star32975@gmail.com>
2021-07-08 12:12:48 +0300

    Added order controller with order Dto, extended mapping profile, added extension for easy usage, implement service order too

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -17,6 +17,8 @@ namespace API.Helpers
             CreateMap<Address, AddressDto>().ReverseMap();
             CreateMap<CustomerBasketDto, CustomerBasket>();
             CreateMap<BasketItemDto, BasketItem>();
+            CreateMap<AddressDto, Core.Entities.OrderAggregate.Address>();
+
         }
     }
 }
--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities;
 using Core.Entities.OrderAggregate;
 using Core.Intefraces;
 
@@ -7,9 +9,38 @@ namespace Infrastructure.Services
 {
     public class OrderService : IOrderService
     {
-        public Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethod, string basketId, Address shippingAddress)
+        private readonly IGenericRepository<Order> _orderRepo;
+        private readonly IGenericRepository<DeliveryMethod> _dmRepo;
+        private readonly IBasketRepository _basketRepo;
+        private readonly IGenericRepository<Product> _productRepo;
+        public OrderService(IGenericRepository<Order> orderRepo, IGenericRepository<DeliveryMethod> dmRepo,
+        IGenericRepository<Product> productRepo, IBasketRepository basketRepo)
         {
-            throw new System.NotImplementedException();
+            _basketRepo = basketRepo;
+            _productRepo = productRepo;
+            _dmRepo = dmRepo;
+            _orderRepo = orderRepo;
+        }
+
+        public async Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethodId, string basketId, Address shippingAddress)
+        {
+            var basket = await _basketRepo.GetBasketAsync(basketId);
+            var items = new List<OrderItem>();
+            foreach (var item in basket.Items)
+            {
+                var productItem = await _productRepo.GetByIdAsync(item.Id);
+                var itemOrdered = new ProductItemOrdered(productItem.Id, productItem.Name, productItem.PictureUrl);
+                var orderItem = new OrderItem(itemOrdered, productItem.Price, item.Quantity);
+                items.Add(orderItem);
+            }
+
+            var deliveryMethod = await _dmRepo.GetByIdAsync(deliveryMethodId);
+
+            var subtotal = items.Sum(item => item.Price * item.Quantity);
+
+            var order = new Order(items, buyerEmail, shippingAddress, deliveryMethod, subtotal);
+
+            return order;
         }
 
         public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
