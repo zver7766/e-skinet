3334ee1ce0a65f0e63dee3941e02cca828d8a72b
Max Prokhorchuk <star32975@gmail.com>
2021-07-08 15:45:44 +0300

    Added & implemented Unit Of Work pattern for Db

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -14,6 +14,7 @@ namespace API.Extensions
         {
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
---
+++ Core/Intefraces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Intefraces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
+
+        Task<int> Complete(); 
+    }
+}
