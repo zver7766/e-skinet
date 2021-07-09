5da130e57aee3ca1f5205109f4743918b77972a3
Max Prokhorchuk <star32975@gmail.com>
2021-07-09 12:55:46 +0300

    Created the delivery form with service |NEED TO SEND AUTH JWT TOKEN|

--- client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
+++ client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { IDeliveryMethod } from 'src/app/shared/models/deliveryMethod';
+import { CheckoutService } from '../checkout.service';
 
 @Component({
   selector: 'app-checkout-delivery',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout-delivery.component.scss']
 })
 export class CheckoutDeliveryComponent implements OnInit {
+  @Input() checkoutForm: FormGroup;
+  deliveryMethods: IDeliveryMethod[];
 
-  constructor() { }
+  constructor(private checkoutService: CheckoutService) { }
 
   ngOnInit(): void {
+    this.checkoutService.getDeliveryMethods().subscribe((dm: IDeliveryMethod[]) => {
+      this.deliveryMethods = dm;
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
