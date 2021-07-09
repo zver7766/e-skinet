6420936ba9ef2ccab48961c2ea6ecbdf6e4c0059
Max Prokhorchuk <star32975@gmail.com>
2021-07-09 10:54:03 +0300

    Checkout page layout

--- client/src/app/checkout/checkout.module.ts
+++ client/src/app/checkout/checkout.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutComponent } from './checkout.component';
 import { CheckoutRoutingModule } from './checkout-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [CheckoutComponent],
   imports: [
     CommonModule,
-    CheckoutRoutingModule
+    CheckoutRoutingModule,
+    SharedModule
   ]
 })
 export class CheckoutModule { }
