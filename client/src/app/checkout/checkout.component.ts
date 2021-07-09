16c99ac0d4993dd0aeb8aec005e0f738aac5f198
Max Prokhorchuk <star32975@gmail.com>
2021-07-09 11:33:09 +0300

    Added the checkout form

--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -9,9 +9,28 @@ import { FormGroup } from '@angular/forms';
 export class CheckoutComponent implements OnInit {
   checkoutForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createCheckoutForm();
   }
 
+  createCheckoutForm() {
+    this.checkoutForm = this.fb.group({
+      addressForm: this.fb.group({
+        firstName: [null, Validators.required],
+        lastName: [null, Validators.required],
+        street: [null, Validators.required],
+        city: [null, Validators.required],
+        state: [null, Validators.required],
+        zipcode: [null, Validators.required]
+      }),
+      deliveryForm: this.fb.group({
+        deliveryMethod: [null, Validators.required],
+      }),
+      paymentForm: this.fb.group({
+        nameOnCard: [null, Validators.required]
+      })
+    });
+  }
 }
