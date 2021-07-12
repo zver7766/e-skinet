afba0b2b16d6edb4ae3afa2d52dc4a84a0938b88
Max Prokhorchuk <star32975@gmail.com>
2021-07-12 21:59:47 +0300

    Pre-publishing adjustment

--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -15,11 +15,19 @@ export class LoadingInterceptor implements HttpInterceptor {
   constructor(private busyService: BusyService) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if (!request.url.includes('emailexists')) {
-      this.busyService.busy();
+    if (request.method === 'POST' && request.url.includes('orders')) {
+      return next.handle(request);
     }
+
+    if (request.method === 'DELETE') {
+      return next.handle(request);
+    }
+    if (!request.url.includes('emailexists')) {
+      return next.handle(request);
+    }
+    this.busyService.busy();
     return next.handle(request).pipe(
-      delay(1000),
+      // delay(1000),
       finalize(() => {
         this.busyService.idle();
       })
