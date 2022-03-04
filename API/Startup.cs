95a8dcd7207dc2a6706310b08d67f800adc0a2f2
Maksym Prokhorchuk <star32975@gmail.com>
2022-03-04 11:57:29 +0200

    Remove comments, test CI

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,8 +22,6 @@ namespace API
             _config = config;
         }
 
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -60,8 +58,7 @@ namespace API
             });
 
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
